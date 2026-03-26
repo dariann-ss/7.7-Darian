a1f8fa7eee80413675463575fae06f2b66adbe8b
dariann-ss <dariansoto2009@gmail.com>
2026-03-26 16:27:09 +0000

    done

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Script Started")
+
+function start(){
+    console.log("click");
+
+    let heading = document.createElement("h1");
+
+    heading.innerText = "Attendance";
+    heading.style.color = "blue";
+    heading.style.textDecoration = "underline";
+
+    document.body.appendChild(heading);
+}
+
+function addName(){
+    
+}
